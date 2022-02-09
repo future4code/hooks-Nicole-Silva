096531fb6cce6e646e09624e1d516a46e0e5cb63
NicoleFersi <nickfersi@hotmail.com>
2022-02-08 22:50:17 -0300

    Todos os meus exercícios

---
+++ modulo-1/projeto-onboarding/aprendizados.md
@@ -0,0 +1,3 @@
+Aprendizados da semana
+
+Essa semana eu aprendi na **Labenu** a mexer no VSCode, no git e no terminal. Aprendi que preciso ser persistente quando a atividade não está dando certo, e não posso ter vergonha de pedir ajuda. E com fé em Deus todos juntos seremos execelentes devs.
---
+++ modulo-1/variaveis/template_javascript/index.js
@@ -0,0 +1,55 @@
+
+//Exercicio de interpretação de código
+
+// 1 - 10
+//     10, 5
+
+// 2 - 10, 10, 10
+
+/* 3 - let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+p - horasPorDia
+t - valorPorDia*/
+
+/*let nome
+let idade 
+
+console.log(typeof nome, typeof idade)*/
+//o tipo das váriaveis é undifined, pois elas não foram definidas
+
+// Exercício de escrita de código
+// 1 - 
+/*let nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual a sua idade?")
+
+console.log(typeof nome, typeof idade)
+o tipo das duas variáveis são strings pois tudo que o usuário insere em um prompt é uma strirng*/
+
+//let nome = prompt("Qual o seu nome?")
+//let idade = prompt("Qual sua idade?")
+//console.log("Olá", nome, "você tem", idade, "anos")
+
+// 2 - 
+/*let estudante = prompt("Você é estudante?")
+let praia = prompt("Você gosta de praia?")
+let azul = prompt("Você gosta de azul?")
+
+console.log("Você é estudante?", estudante)
+console.log("Você gosta de praia?", praia)
+console.log("Você gosta de azul?", azul)*/
+
+//3 -
+/*let a = 10
+let b = 25
+let c = 10
+a = b
+b = c
+c = a
+console.log("O novo valor de a é: ", a)
+console.log("O novo valor de b é: ", b)*/
+
+
+
+
