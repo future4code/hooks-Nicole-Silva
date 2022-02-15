2977f2a0673b52757a26c29b7781efd68ab71045
NicoleFersi <nickfersi@hotmail.com>
2022-02-15 19:18:19 -0300

    strings-arrays

---
+++ strings-arrays/template_javascript/index.js
@@ -0,0 +1,53 @@
+//Exercício de interpretação de código
+//1 - 
+
+//let array
+//console.log('a. ', array)
+// a. undifined
+
+//array = null
+//console.log('b. ', array)
+// b. null
+
+//array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//console.log('c. ', array.length)
+//c. 11
+
+//let i = 0
+//console.log('d. ', array[i])
+//d. 3
+
+//array[i+1] = 19
+//console.log('e. ', array)
+//e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+//const valor = array[i+6]
+//console.log('f. ', valor)
+//f. 9
+
+//2 - 
+//const frase = prompt("Digite uma frase")
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//SUBI NUM ÔNIBUS NO MIRROCOS 27
+
+// Exercìcio de escrita de código
+
+//const emailDoUsuario = prompt("Digite seu e-mail")
+//const nomedoUsuario = prompt ("Nome do Usuário")
+//const concatenação = " O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vinda(o) " + nomedoUsuario +"!"
+//console.log(concatenação)
+
+//const comidaPreferida = [ "lasanha", "camarão", "strogonoff", "pizza", "purê"]
+//console.log(comidaPreferida)
+
+//const listaComidas = ("Essas são as minhas comidas favoritas"[])
+//console.log[listaComidas]
+
+//const listaDeTarefas = []
+//const tarefa1 = prompt("Digite uma tarefa para realizar no dia")
+//const tarefa2 = prompt("Digite outra tarefa para realizar no dia")
+//const tarefa3 = prompt("Digite uma útima tarefa para realizar no dia")
+//const listaDeTarefas = [tarefa1, tarefa2, tarefa3]
+//console.log[listaDeTarefas]
+
