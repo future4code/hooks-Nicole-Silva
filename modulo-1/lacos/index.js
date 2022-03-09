da9852be6991fdebc318f02d54d5cba12dda7b64
Nicole Ferreira <nickfersi@hotmail.com>
2022-03-08 21:25:58 -0300

    laços

---
+++ modulo-1/lacos/index.js
@@ -0,0 +1,45 @@
+//Exercícios de interpertração de códigos
+// 1 - 10
+
+// 2 - 
+// a - 19, 21, 23, 25, 27, 30
+// b - 
+
+// 3 - 
+// *
+// **
+// ***
+// ****
+
+//Exercícios de escrita de código
+// 1 - 
+
+// let nomeDosPets = [] ;
+
+// const pergunta = Number(prompt("Quantos bichinhos de estimação você tem?"))
+
+// const qtdePetUsuario = 0
+
+// console.log(qtdePetUsuario)
+
+// function bichos() {
+//     if(pergunta === 0) {
+//         console.log("Que pena! Você pode adotar um pet!")
+//     }else{
+//         while(qtdePetUsuario < pergunta){
+//             nomeDosPets = (prompt("Digite o nome do seu pet."))
+//             qtdePetUsuario++
+//         }
+//         console.log(nomeDosPets)
+//     }
+// }
+// bichos()
+
+// 2 - 
+
+let arrayDeNumeros = [ 2, 4, 7, 10, 12, 15, 20] ;
+//console.log(arrayDeNumeros)
+
+console.log(arrayDeNumeros)
+
+
