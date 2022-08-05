e540e6a7372383ef3086feee1cbcdceb2ae4452e
Nicole Ferreira <nickfersi@hotmail.com>
2022-08-04 23:29:31 -0300

    node-package-json

---
+++ modulo 5/node-package-json/Ex2/index.js
@@ -0,0 +1,16 @@
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+const add = num1 + num2;
+console.log(`Soma: ${add}`);
+
+const sub = num1 - num2;
+console.log(`Subtração: ${sub}`);
+
+const mult = num1 * num2;
+console.log(`Multiplicação: ${mult}`);
+
+const div = num1 / num2;
+console.log(`Divisão: ${div}`);
+
+  
