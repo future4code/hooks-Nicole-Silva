88dc3db4c00b7fc6bfdab3289a862001a2330392
Nicole Ferreira <nickfersi@hotmail.com>
2022-09-12 21:16:46 -0300

    aprofundamento-express

---
+++ modulo 5/aprofundamento-express/src/data.ts
@@ -0,0 +1,39 @@
+export type tarefas = {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
+export const ToDo = [
+    {
+      "userId": 1,
+      "id": 123,
+      "title": "Estudar",
+      "completed": true
+    },
+    {
+      "userId": 2,
+      "id": 234,
+      "title": "Ir ao mercado",
+      "completed": false
+    },
+    {
+      "userId": 3,
+      "id": 345,
+      "title": "Lavar a roupa",
+      "completed": true
+    },
+    {
+      "userId": 4,
+      "id": 456,
+      "title": "Ir ao banco",
+      "completed": true
+    },
+    {
+      "userId": 5,
+      "id": 567,
+      "title": "Assistir Eu te amo, agora morra: o caso Michelle Carter",
+      "completed": false
+    },
+]
